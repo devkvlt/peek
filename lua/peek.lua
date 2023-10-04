2af58e657290554fb9b1ef5d96f264cb41b75343
devkvlt <devkvlt@pm.me>
2023-10-04 16:50:37 +0100

    Initial commit

---
+++ lua/peek.lua
@@ -0,0 +1,47 @@
+return function()
+  local params = vim.lsp.util.make_position_params()
+  local result = vim.lsp.buf_request_sync(0, 'textDocument/definition', params, 1000)
+
+  if not result then
+    return
+  end
+
+  local locations = result[1].result
+
+  if not locations or vim.tbl_isempty(locations) then
+    return
+  end
+
+  local uri = locations[1].uri or locations[1].targetUri
+  local range = locations[1].range or locations[1].targetRange
+
+  local bufnr = vim.uri_to_bufnr(uri)
+
+  local win_opts = {
+    width = 85,
+    height = 15,
+    relative = 'cursor',
+    row = 1,
+    col = 1,
+    border = { '╭', '─', '╮', '│', '╯', '─', '╰', '│' },
+    title = ' ' .. vim.fn.expand('<cword>') .. ' ',
+    title_pos = 'center',
+  }
+
+  local winid = vim.api.nvim_open_win(bufnr, true, win_opts)
+
+  vim.api.nvim_win_set_option(winid, 'winhl', 'Normal:Normal,FloatBorder:WinSeparator')
+  vim.api.nvim_win_set_cursor(winid, { range.start.line + 1, range.start.character }) -- Set the cursor position in the new window
+  vim.api.nvim_buf_set_option(bufnr, 'modifiable', false)
+  vim.opt_local.signcolumn = 'no'
+
+  vim.keymap.set('n', 'q', function()
+    local id = vim.fn.win_getid()
+
+    if vim.api.nvim_win_get_config(id).relative ~= '' then
+      vim.keymap.set('n', 'q', 'q', { buffer = 0 })
+      vim.api.nvim_buf_set_option(bufnr, 'modifiable', true)
+      vim.api.nvim_win_close(id, false)
+    end
+  end, { buffer = 0 })
+end
